1be78539d76161cf725350ac28da4030565479ac
Ricardo <902401130@portic.ipp.pt>
2025-01-26 23:29:35 +0000

    Small changes made on SignUp click

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from "@angular/router";
 import { SignUpComponent } from "./sign-up/sign-up.component";
 
 export const routes: Routes = [
-    {path: 'SignUp', component: SignUpComponent}
-]
+    {path: 'SignUp', component: SignUpComponent},
+];
