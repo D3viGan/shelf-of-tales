221bdf581a4908183f8ffc732c2f3f7d15c1a701
Ricardo <902401130@portic.ipp.pt>
2025-01-26 21:09:14 +0000

    Created user service

---
+++ src/app/Services/user.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../Models/user';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private readonly theEndPoint = "http://shelfoftales.onrender.com/api/user"
+
+  constructor(private myWebApiClient: HttpClient) { 
+  }
+
+  // Create new user
+  createUser(newUser: User): Observable<any> {
+    return this.myWebApiClient.post<any>(`${this.theEndPoint}/signup`, newUser); // POST Request to create a user
+  }
+}
