04782e41ce4ae50bf018f9b1120ddcf293e1ddf1
Ricardo <902401130@portic.ipp.pt>
2025-01-27 00:47:56 +0000

    Sign in button added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from "@angular/router";
 import { SignUpComponent } from "./sign-up/sign-up.component";
+import { SignInComponent } from "./sign-in/sign-in.component";
 
 export const routes: Routes = [
-    {path: 'SignUp', component: SignUpComponent},
+    { path: 'SignUp', component: SignUpComponent },
+    { path: 'SignIn', component: SignInComponent },
 ];
