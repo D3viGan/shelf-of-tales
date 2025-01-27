2398fdf8396388bc1f80cd4f0bb69d2dd461c680
Ricardo <902401130@portic.ipp.pt>
2025-01-27 00:26:34 +0000

    Auth/token update 1

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, catchError, map, Observable, throwError } from 'rxjs';
 import { User } from '../Models/user';
+import { HttpClient } from '@angular/common/http';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +13,34 @@ export class AuthService {
   private userSubject = new BehaviorSubject<User | null>(null);
   public user!: Observable<User | null>;
   
-  constructor() {
+  constructor(private http: HttpClient, private tokenService: TokenService) {
+    if(this.tokenService.hasToken('user')){
+      this.userSubject.next(JSON.parse(this.tokenService.getToken('user')) );
+    }
+  }
+
+  signIn(email: string, password: string){
+    return this.http.post<any>(`${this.endpoint}/signIn`, { email, password })
+    .pipe(
+      map(user=>this.tokenService.saveToken('user', JSON.stringify(user))),
+      catchError(this.handleError<User>("signIn"))
+    );
+  }
+
+  logout() {
+     this.tokenService.deleteToken('user');
+  }
+
+  hasToken() {
+    return this.tokenService.hasToken('user');
+  }
+
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error:any): Observable<T> => {
+      this.logout();
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return throwError(() => new Error(error.message))
+;    }
   }
 }
--- src/app/Services/token.service.ts
+++ src/app/Services/token.service.ts
@@ -4,6 +4,26 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TokenService {
+  saveToken(key: string, token: string) {
+    localStorage.setItem(key, token);
+  }
 
-  constructor() { }
+  deleteToken(key:string) {
+    localStorage.removeItem(key);
+  }
+
+  getToken(key:string) {
+    return localStorage.getItem(key) ?? '';
+  }
+
+  hasToken(key:string) {
+    if(typeof localStorage == 'undefined') return false;
+    return !!this.getToken(key);
+  }
+
+  getRole(key:string) {
+    if(this.getToken(key)!='') {
+      return JSON.parse(this.getToken(key)).role;
+    }
+  }
 }
