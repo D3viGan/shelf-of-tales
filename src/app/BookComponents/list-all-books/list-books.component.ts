d89cd858929bdaa8b8e89980ae0f683b1c7d9966
DeviGan31 <902401130@portic.ipp.pt>
2025-02-15 16:36:25 +0000

    List book css updated

--- src/app/BookComponents/list-all-books/list-books.component.ts
+++ src/app/BookComponents/list-all-books/list-books.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../../Services/book.service';
 import { CommonModule, NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list-books',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './list-books.component.html',
   styleUrl: './list-books.component.css'
 })
 
 export class ListBooksComponent implements OnInit {
   books: any[] = [];
+isLoading: any;
+errorMessage: any;
 
   constructor(private bookService: BookService) {}
 
