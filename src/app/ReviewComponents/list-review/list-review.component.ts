7424af1062c995c348e1c2e2294cb9c39fadc991
DeviGan31 <902401130@portic.ipp.pt>
2025-02-15 14:43:44 +0000

    reate and list revicew

--- src/app/ReviewComponents/list-review/list-review.component.ts
+++ src/app/ReviewComponents/list-review/list-review.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { GetReview } from '../../Models/get-review';
+import { ReviewService } from '../../Services/review.service';
+import { ActivatedRoute } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-list-review',
-  imports: [],
+  selector: 'app-listreview',
+  standalone: true,
+  imports: [NgIf,NgFor],
   templateUrl: './list-review.component.html',
   styleUrl: './list-review.component.css'
 })
-export class ListReviewComponent {
+export class ListreviewComponent {
+  isbn!: string;
+  reviews: GetReview[] = [];
+  errorMessage: string | null = null;
 
+  constructor(private reviewService: ReviewService, public route: ActivatedRoute) { }
+
+  ngOnInit(): void { this.route.params.subscribe(params => this.isbn=params['isbn'])
+    if (this.isbn) {
+      this.loadReviews();
+    }
+  }
+
+  loadReviews(): void {
+    this.reviewService.getAllReviews(this.isbn).subscribe({
+      next: (data) => {
+        this.reviews = data;
+      },
+      error: (err) => {
+        console.error('Error fetching reviews:', err);
+        this.errorMessage = 'Unable to load reviews.';
+      }
+    });
+  }
 }
